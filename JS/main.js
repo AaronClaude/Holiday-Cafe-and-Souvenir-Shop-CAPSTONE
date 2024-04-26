4a4fbab68d1902d114d5aaea9340f10780fd57ec
Aaron Claude Lacap <aaronclaude0028@gmail.com>
2024-04-26 16:54:07 +0800

    update html, css
    - mobile responsive (unfinished)
    - added mobile sidebar

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,5 @@
 
-// JavaScript function to hide/show the navbar on scroll
-let prevScrollpos = window.pageYOffset;
 
-window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.querySelector("header").style.top = "0";
-    } else {
-        document.querySelector("header").style.top = "-75px"; // Adjust the height of the navbar as needed
-    }
-    prevScrollpos = currentScrollPos;
-}
 
 // Script for toggle menu (responsive mobile view)
 const toggleMenu = document.querySelector('.toggle-menu');
