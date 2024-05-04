3bea42308791c77cb66f0469b035c5b60f54fc26
Ace.Dev <94028925+AaronClaude@users.noreply.github.com>
2024-05-05 01:22:03 +0800

    Organizing files

--- RestaurantPOS/Restro/cashier/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['staff_id']);
-session_destroy();
-header("Location: ../../index.php");
-exit;
--- RestaurantPOS/Restro/customer/config/config.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-$dbuser = "root";
-$dbpass = "";
-$host = "localhost";
-$db = "rposystem";
-$mysqli = new mysqli($host, $dbuser, $dbpass, $db);
-?>
