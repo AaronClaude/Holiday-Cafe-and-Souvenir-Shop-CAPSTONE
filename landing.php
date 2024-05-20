86918626881be1cee25e01289d527ada3fb4d800
hyehyess <166274338+hyehyess@users.noreply.github.com>
2024-05-20 14:37:09 +0800

    update

---
+++ landing.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Holiday Cafe Souvenir Shop</title>
+    <link rel="stylesheet" href="/CSS/landing.css">
+    
+     <!-- Poppins font -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
+   
+
+
+</head>
+
+<body>
+    <div class="top-logo">
+        <img src="img/logos/nav-logo.png">
+    </div>
+    <h3>HCSS PORTAL</h3>
+
+    <div class="main-logo">
+        <img src="img/logos/main-logo.png">
+    </div>
+
+    
+    <div class="box">
+        <div class="option">
+            Log in as
+        </div>
+        <a href="PHP/cashierlogin.php" id="btn1">
+            <img src="IMG/icons/cashier.png">
+        </a>
+
+        <a href="PHP/adminlogin.php" id="btn2">
+            <img src="IMG/icons/admin.png">
+        </a>
+
+    </div>
+   <div class="MV">
+    <p><b>Mission:</b>  To craft unforgettable festive experiences for families and friends, 
+        making every visit a joyful and affordable celebration. </p>
+
+        <br>
+    <p><b>Vision:</b>  To become the global destination
+         where everyday feels like a holiday.</p>
+
+   </div>
+</body>
+</html>
